6980f920b9b912d1f111d776074183adece4a0c1
ramonskie <ramonmakkelie@gmail.com>
2018-03-29 12:34:23 +0200

    let me fail

--- blue-green-app-deployment/bgd-app/tests/NumberBlackBox-tests.js
+++ blue-green-app-deployment/bgd-app/tests/NumberBlackBox-tests.js
@@ -18,7 +18,7 @@ describe('NumberBlackBox Unit tests', function() {
   });
 
   it('add() should return NaN if a NaN value is passed in as argument', function() {
-    expect(numberBlackBox.add('hello')).to.be.NaN;
+    expect(numberBlackBox.add('1')).to.be.NaN;
   });
 
   // subtract
