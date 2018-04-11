4e6a960ed24a60db18b41b39d1b3ce6dbc49aa82
Ruben Koster <superruup@gmail.com>
2018-04-11 10:55:06 +0200

    Integrated into deployment repo

--- create-uaa.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash
-
-uaac client add gitlab \
-  --scope=scim.userids openid oauth.approvals \
-  --client_id: gitlab \
-  --authorized_grant_types=refresh_token authorization_code \
-  --redirect_uri=http://10.244.0.2/users/auth/oauth2_generic/callback \
-  --authorities: uaa.none
