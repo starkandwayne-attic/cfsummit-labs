c79e04b43c797dd8a828c05c308b343e1dbce488
Ruben Koster <rkoster@starkandwayne.com>
2018-04-12 14:35:50 +0200

    Make repipe script pwd independent

--- merge-request-pipeline/ci/repipe.sh
+++ merge-request-pipeline/ci/repipe.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+ci_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 remote="$(git remote -v | sed -e 's/origin\s*\(.*\) .*/\1/g' | head -n1).git"
 prefix=$(echo $USER | cut -d@ -f1)
 
 set -x
-fly -t lab set-pipeline -p "${prefix}-merge-request-pipeline" -c ci/pipeline.yml \
+fly -t lab set-pipeline -p "${prefix}-merge-request-pipeline" -c ${ci_dir}/pipeline.yml \
     -v git_remote=${remote}
