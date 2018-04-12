b262d66cdc8d6508edc68f15c9a9753d8dec22dd
Ruben Koster <rkoster@starkandwayne.com>
2018-04-12 15:32:05 +0200

    Drop .git from remote url

--- merge-request-pipeline/ci/repipe.sh
+++ merge-request-pipeline/ci/repipe.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ci_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-remote="$(git remote -v | sed -e 's/origin\s*\(.*\) .*/\1/g' | head -n1).git"
+remote="$(git remote -v | sed -e 's/origin\s*\(.*\) .*/\1/g' | head -n1)"
 prefix=$(echo $USER | cut -d@ -f1)
 
 set -x
