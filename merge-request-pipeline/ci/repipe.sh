6a51bd0cf5d91afddd3cb2edf891e04a219715a9
Ruben Koster <rkoster@starkandwayne.com>
2018-04-12 16:22:54 +0200

    Unpause pipeline in repipe

--- merge-request-pipeline/ci/repipe.sh
+++ merge-request-pipeline/ci/repipe.sh
@@ -3,7 +3,10 @@
 ci_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 remote="$(git remote -v | sed -e 's/origin\s*\(.*\) .*/\1/g' | head -n1)"
 prefix=$(echo $USER | cut -d@ -f1)
+pipeline="${prefix}-merge-request-pipeline"
 
 set -x
-fly -t lab set-pipeline -p "${prefix}-merge-request-pipeline" -c ${ci_dir}/pipeline.yml \
+fly -t lab set-pipeline -p "${pipeline}" -c ${ci_dir}/pipeline.yml \
     -v git_remote=${remote}
+
+fly -t lab unpause-pipeline -p "${pipeline}"
